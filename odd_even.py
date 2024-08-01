53bfca248e610b1b001f8dc0e96b970d3f65ed5a
Hariharan Achary <hariachary7@gmail.com>
2024-08-01 10:13:15 +0530

    Added Odd Even Program

--- odd_even.py
+++ odd_even.py
@@ -2,19 +2,28 @@
 
 def validate_even_odd(number):
     try:
-        if (number % 2) == 0:
-            print(f"The number {number} is even.")
-
-        else:
-            print(f"The number {number} is odd.")
+        # Ternary operation if the number is even return True else False
+        return True if (number % 2) == 0 else False
 
     except Exception as ex:
         print(ex)
 
 
+# Execution start
+# Used try and catch block to handle error exception while taking the input
+# If input type is not of integer then it will go into except block
 try:
+    # Input keyword is used to take number from command line (Default type String) int() is used to convert into integer.
     input_num = int(input("Please Enter a number: "))
+
+    # Calling the function which is defined above and passing the input received from the commandline.
     validate_even_odd(input_num)
+    if validate_even_odd:
+        print(f"The number {input_num} is even.")
+    else:
+        print(f"The number {input_num} is odd.")
+
 
 except ValueError:
+    # If value error is raised during the execution of try block it will be traced here.
     print("Only numbers are allowed.")
