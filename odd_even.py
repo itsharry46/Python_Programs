87920f99c61f530a8ecda6a056b5ff27c3d46779
Hariharan Achary <hariachary7@gmail.com>
2024-08-01 10:59:48 +0530

    Added Positive Negative number
    Write a program to find the given number is positive or negative

--- odd_even.py
+++ odd_even.py
@@ -1,4 +1,4 @@
-# Write a program to validate given number is odd or even.
+# Write a program to validate given number is odd or even
 
 def validate_even_odd(number):
     try:
@@ -13,17 +13,17 @@ def validate_even_odd(number):
 # Used try and catch block to handle error exception while taking the input
 # If input type is not of integer then it will go into except block
 try:
-    # Input keyword is used to take number from command line (Default type String) int() is used to convert into integer.
+    # Input keyword is used to take number from command line (Default type String) int() is used to convert into integer
     input_num = int(input("Please Enter a number: "))
 
-    # Calling the function which is defined above and passing the input received from the commandline.
-    validate_even_odd(input_num)
-    if validate_even_odd:
+    # Calling the function which is defined above and passing the input received from the commandline
+    result = validate_even_odd(input_num)
+    if result:
         print(f"The number {input_num} is even.")
     else:
         print(f"The number {input_num} is odd.")
 
 
 except ValueError:
-    # If value error is raised during the execution of try block it will be traced here.
+    # If value error is raised during the execution of try block it will be traced here
     print("Only numbers are allowed.")
---
+++ positive_negative.py
@@ -0,0 +1,29 @@
+# Write a program to find the given number is positive or negative
+
+# Defining the function with parameter input of number dataType
+def positive_negative(num: int):
+
+    # If the number is 0 marking it as neutral number
+    if num == 0:
+        return f"The number {num} is neutral"
+
+    # If the above condition does not satisfy then below condition will execute. Validating the number is greater than 0 or not
+    elif num > 0:
+        return f"The number {num} is positive"
+
+    # If all the above condition is not satisfactory then else block get executed
+    else:
+        return f"The number {num} is negative"
+
+
+try:
+    # Input keyword is used to take number from command line (Default type String) int() is used to convert into integer
+    input_num = int(input("Please enter a number: "))
+
+    # Calling the function which is defined above and passing input_num as parameter, which is received from the commandline
+    result = positive_negative(input_num)
+    print(result)
+
+except ValueError:
+    # If value error is raised during the execution of try block it will be traced here
+    print("Only numbers are allowed.")
